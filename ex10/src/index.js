7200a010274f3c2998f0323d9f3cd78469d75e95
unknown <edina.kurtic90@hotmail.com>
2021-03-11 00:45:51 +0100

    dodan ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var product;
+
+    product = numOne * numTwo;
+    
+    // Only change code above this line
+    return product;
+}
+
+console.log(main(25, 8));  // change this line
+
+module.exports = main;
